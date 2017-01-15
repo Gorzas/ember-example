054d1353d8e243ea8c0a6eae3600a5904767e97f
Gorzas <joseda87@gmail.com>
2017-01-15 19:48:12 +0100

    Include routes of the system

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('posts', function() {
+    this.route('new');
+    this.route('show', { path: '/:post_id' });
+    this.route('edit', { path: 'edit/:post_id' });
+  });
 });
 
 export default Router;
