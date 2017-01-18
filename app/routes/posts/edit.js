aad9424473df99ddff1adbb14e8d58992f125f89
Gorzas <joseda87@gmail.com>
2017-01-18 20:51:08 +0100

    Allow edition of blog posts

--- app/routes/posts/edit.js
+++ app/routes/posts/edit.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    submit(model) {
+      model.save().then(
+        () => {
+          this.transitionTo('posts');
+        }
+      ).catch(
+        () => {
+          // what if the API call fails?
+        }
+      );
+    }
+  },
 });
